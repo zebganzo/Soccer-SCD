0df4e3f22ea5b3e2f9dbd44b67c405e1e493ad4a
Alessandro <neffix>
2013-09-26 15:47:52 +0200

    Updated Keeper.pl

--- keeper.pl
+++ keeper.pl
@@ -68,8 +68,9 @@ action(CellX, CellY, move) :-
 		current_predicate(reference_position/2), !,								% checks if the player has a fixed assigned position
 		reference_position(RefX, RefY),											% get the player assigned position
 		move_to_pos(RefX, RefY, CellX, CellY)									% move to the closest cell to the  fixed assigned position
-		;																		% or if the player has not a fixed assigned position 																										
-		player(position(CellX, CellY), _, _, _)									% he doesn't need to move
+		;
+		goal_kick_position(GKickX, GKickY),
+		move_to_pos(GKickX, GKickY, CellX, CellY)
 	).
 
 % Move action if the game is in status 'blocked', caused by an event (free kick, penalty, corner ...)
